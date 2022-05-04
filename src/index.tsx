e3374ec23bf50ca1c4c0d02aaa5af145be57b5c6
Jimmy Liikala <jimmy.liikala@gmail.com>
2022-05-04 22:51:40 +0200

    Removed commented import

--- src/index.tsx
+++ src/index.tsx
@@ -2,7 +2,6 @@ import React from 'react';
 import { createRoot } from 'react-dom/client';
 import { Provider } from 'react-redux';
 import { store } from 'app/store';
-// import 'dotenv/config'
 import App from 'App';
 import 'index.css';
 
