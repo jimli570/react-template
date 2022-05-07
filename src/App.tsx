a83eb3ff392c64adf411027877c025d86f406a53
Jimmy Liikala <jimmy.liikala@gmail.com>
2022-05-07 23:00:53 +0200

    Cleanups

--- src/App.tsx
+++ src/App.tsx
@@ -8,82 +8,80 @@ import {
 
 function Home() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <Counter />
-        <p>
-          You are in
-          {' '}
-          <code>{process.env.NODE_ENV}</code>
-          {' '}
-          mode.
-        </p>
+    <header className="App-header">
+      <img src={logo} className="App-logo" alt="logo" />
+      <Counter />
+      <p>
+        You are in
+        {' '}
+        <code>{process.env.NODE_ENV}</code>
+        {' '}
+        mode.
+      </p>
 
-        <p>
-          REACT_APP_BACKEND is set to
-          {' '}
-          <code>{process.env.REACT_APP_BACKEND}</code>
-          {' '}
-          in the .env files.
-        </p>
+      <p>
+        REACT_APP_BACKEND is set to
+        {' '}
+        <code>{process.env.REACT_APP_BACKEND}</code>
+        {' '}
+        in the .env files.
+      </p>
 
-        <p>
-          Edit
-          {' '}
-          <code>src/App.tsx</code>
-          {' '}
-          and save to reload.
-        </p>
+      <p>
+        Edit
+        {' '}
+        <code>src/App.tsx</code>
+        {' '}
+        and save to reload.
+      </p>
 
-        <p>
-          Routes:
-          <Link to="/">Home</Link>
-          &nbsp;
-          <Link to="/other">Other home</Link>
-        </p>
+      <p>
+        Routes:
+        <Link to="/">Home</Link>
+        &nbsp;
+        <Link to="/other">Other home</Link>
+      </p>
 
-        <span>
-          <span>Learn </span>
-          <a
-            className="App-link"
-            href="https://reactjs.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            React
-          </a>
-          <span>, </span>
-          <a
-            className="App-link"
-            href="https://redux.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Redux
-          </a>
-          <span>, </span>
-          <a
-            className="App-link"
-            href="https://redux-toolkit.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Redux Toolkit
-          </a>
-          ,
-          <span> and </span>
-          <a
-            className="App-link"
-            href="https://react-redux.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            React Redux
-          </a>
-        </span>
-      </header>
-    </div>
+      <span>
+        <span>Learn </span>
+        <a
+          className="App-link"
+          href="https://reactjs.org/"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          React
+        </a>
+        <span>, </span>
+        <a
+          className="App-link"
+          href="https://redux.js.org/"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Redux
+        </a>
+        <span>, </span>
+        <a
+          className="App-link"
+          href="https://redux-toolkit.js.org/"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Redux Toolkit
+        </a>
+        ,
+        <span> and </span>
+        <a
+          className="App-link"
+          href="https://react-redux.js.org/"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          React Redux
+        </a>
+      </span>
+    </header>
   );
 }
 
@@ -95,11 +93,13 @@ function Other() {
 
 export default function App() {
   return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/other" element={<Other />} />
-      </Routes>
-    </Router>
+    <div className="App">
+      <Router>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/other" element={<Other />} />
+        </Routes>
+      </Router>
+    </div>
   );
 }
